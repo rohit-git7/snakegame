49affc4ded463d08985fa3788c55aed559f962a7
Rohit Takhar <rohit.takhar1994@gmail.com>
2015-01-13 22:01:01 +0530

    Update snake.h

--- snake.h
+++ snake.h
@@ -1,37 +1,37 @@
 #include<iostream>
 #include<ncurses.h>
 #include<cstdlib>
-#include<vector>
+#include<vector>        
 #include<cstdio>
 #include<cstring>
 #include<fcntl.h>
 struct food_val
 {
-int x,y;
-food_val();
-food_val(int,int);
+        int x,y;
+        food_val();
+        food_val(int,int);
 };
 
 class snakeclass
 {
-  int points,temp,coll,del,maxwidth,maxheight,high_score;
-  char direction,snake_char,obst_char,food_char,low_bound;
-  bool get;
-  food_val food;
-  std::vector<std::pair<int,int> > snake;
-  void putfood();
-  std::vector<std::pair<int,int> > obst;
-  bool collision();
-  void movesnake();
+        int points,temp,coll,del,maxwidth,maxheight,high_score;
+        char direction,snake_char,obst_char,food_char,low_bound;
+        bool get;
+        food_val food;
+        std::vector<std::pair<int,int> > snake;
+        void putfood();
+        std::vector<std::pair<int,int> > obst;
+        bool collision();
+        void movesnake();
  
  
- public:
- static int stage;
-  snakeclass(int,int);
-  snakeclass(int);
-  ~snakeclass();
-  void start();
-  void stage1();  
-  void stage2();  
+public:
+        static int stage;
+        snakeclass(int,int);
+        snakeclass(int);
+        ~snakeclass();
+        void start();
+        void stage1();  
+        void stage2();  
 
 };
