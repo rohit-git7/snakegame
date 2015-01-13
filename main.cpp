3ff469d33a5333db997a886ea61217a8b5281a07
Rohit Takhar <rohit.takhar1994@gmail.com>
2015-01-13 21:59:23 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,18 +4,19 @@ int main()
 {
 
  	char buff[10];
-  int i=0,n;
-  long long int sum=0;
-  int fd=open("high.txt",O_RDONLY);
-  n=read(fd,buff,1024);
-  while(i < n-1)
-  {
-    sum= sum*10 +buff[i]-'0';
-    i++;
-  }
-  close(fd);
+ 	int i=0,n;
+	long long int sum=0;
+	int fd=open("high.txt",O_RDONLY);
+	n=read(fd,buff,1024);
+	while(i < n-1)
+	{
+ 		sum= sum*10 +buff[i]-'0';
+ 		i++;
+ 	}
+ 	
+ 	close(fd);
   
-  snakeclass s(sum);
-  s.start();
-  return 0;
+ 	snakeclass s(sum);
+ 	s.start();
+ 	return 0;
 }
