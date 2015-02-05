c4bd1ee298cbb5516e0ad112d5fde0358bd98349
Rohit Takhar <rohit.takhar1994@gmail.com>
2015-02-06 00:19:46 +0530

    Update snake_file.cpp

--- snake_file.cpp
+++ snake_file.cpp
@@ -24,13 +24,13 @@ snakeclass::snakeclass(int pre_point,int high)// snakeclass constructor with two
 	nodelay(stdscr,true);  //getch will not wait until user presses a key
 	keypad(stdscr,true);
 	noecho();
-	curs_set(0);
-	getmaxyx(stdscr,maxheight,maxwidth);
+	curs_set(0);// to hide cursor
+	getmaxyx(stdscr,maxheight,maxwidth);// to get dimensions of window
 
-	snake_char='x';
-	obst_char=(char)219;
-	food_char='*';
-	low_bound='-';
+	snake_char='x';//character to draw snake
+	obst_char=(char)219;// character to draw obstacles
+	food_char='*';// character to draw bait
+	low_bound='-';// character to draw lower boundary
 	food.x=0;
 	food.y=0;
 
@@ -50,70 +50,68 @@ snakeclass::snakeclass(int pre_point,int high)// snakeclass constructor with two
 	stage++;
 }
 
-snakeclass::~snakeclass()
+snakeclass::~snakeclass()// default destructor
 {
 	nodelay(stdscr,false);
 	getch();
 	endwin();
 }
 
-void snakeclass::putfood()
+void snakeclass::putfood()// putting bait at a random place 
 {
 	while(1)
-  	{
+  	{	// getting a point on screen
 		int tmpx=rand()%maxheight+1;
 		int tmpy=rand()%maxwidth+1;
-		for(int i=0;i<snake.size();i++)
+		for(int i=0;i<snake.size();i++)// checking if point lies on snake
 		{ 
-			if( snake[i].first==tmpy && snake[i].second==tmpx)
+			if( snake[i].first==tmpy && snake[i].second==tmpx)// if point lies on snake, generating another position
  				continue;
 	  	}
       		
-      		for(int i=0;i<obst.size();i++)
+      		for(int i=0;i<obst.size();i++)// checking if point lies on any of obstacles
           	{
-            		if(obst[i].second==tmpy && obst[i].first==tmpx)
+            		if(obst[i].second==tmpy && obst[i].first==tmpx)//if point lies on any of obstacles, generating another position
                 		continue;
           	}
 
-		if(tmpy>maxwidth-1 || tmpx > maxheight-3)
+		if(tmpy>maxwidth-1 || tmpx > maxheight-3)// point should lie within window
 			continue;
 	
-		if(stage==2)
-	 	{
-			if(tmpy>maxwidth-2 || tmpx > maxheight-4)
-                		continue;
-	   	}
 		
 		food.x=tmpy;
 		food.y=tmpx;
         	break;
     	}
 	
-	move(food.y,food.x);
-	addch(food_char);
+	move(food.y,food.x);// move cursor to position
+	addch(food_char);//put bait
 	refresh();
   
 }
 
-bool snakeclass::collision()
+bool snakeclass::collision()//to check if there is a collision
 {
+	// if snake collides the boundary, then it should come out from opposite side of window
  	if((snake[0].first==0 && direction=='l')||(snake[0].first==maxwidth-1 && direction=='r') || (snake[0].second==0 &&direction=='u') || (snake[0].second== maxheight-3 && direction=='d'))
    	{
 		temp=1;
     	}
   
-  	for(int i=2; i<snake.size();i++)
+  	for(int i=2; i<snake.size();i++)// if snake collides with itself
     	{
 		if(snake[0].first==snake[i].first && snake[0].second==snake[i].second)
              		return true;
     	}
 
-   	for(int i=0;i<obst.size();i++)
+   	for(int i=0;i<obst.size();i++)//if snake collides with any of obstacles
     	{
     		if(snake[0].first==obst[i].second && snake[0].second==obst[i].first)
              		return true; 
        	}
-     
+     /*
+     snake length increases by one unit at each bait it eats. We need to reset get value after it eats bait.
+     */
 	if(points%10==0 && points!=0)
 	 {
 	 	get=false;
@@ -122,10 +120,10 @@ bool snakeclass::collision()
    	if(snake[0].first==food.x && snake[0].second==food.y)
    	{
 		get=true;
-		putfood();
+		putfood();// putting bait
 		points+=10;
 		
-		attron(A_BOLD);
+		attron(A_BOLD);// bold attribute on
 		move(maxheight-1,0);
 		printw("Your Score:%d",points);
 		if(points > high_score)
@@ -140,7 +138,7 @@ bool snakeclass::collision()
 		attroff(A_BOLD);
 		refresh();
 		
-		if(points%50 == 0)
+		if(points%50 == 0)// increasing speed of snake after every 50 points
  		{
 			del-=8000;  
 		}
@@ -154,9 +152,9 @@ bool snakeclass::collision()
  	return false;
 }
 
-void snakeclass::movesnake()
+void snakeclass::movesnake()// to control snake movement
 {
-  	int tmp=getch();
+  	int tmp=getch();// getting input
   	switch(tmp)
   	{
 		case KEY_LEFT: if(direction!='r')
@@ -176,7 +174,7 @@ void snakeclass::movesnake()
 				  break;
     	}
         
-        if(collision())
+        if(collision())// if there is any collision, set coll variable 1
         {
 		clear();
 		attron(A_BOLD);
@@ -190,7 +188,8 @@ void snakeclass::movesnake()
         	refresh();
 	  	coll=1;
         }
-
+	//We will check the value of coll in the next cycle of movesnake and if it is 1 we will stop execution
+	//if coll is 1, don't draw snake
 	if(coll!=1)
 {   		if(!get)
      		{
@@ -199,7 +198,7 @@ void snakeclass::movesnake()
 			refresh();
 			snake.pop_back();
        		}
-     		else
+     		else// to increase snake length
       		{
 			move(snake[snake.size()-1].second,snake[snake.size()-1].first);
         		addch(' ');
@@ -219,7 +218,7 @@ void snakeclass::movesnake()
 			snake.insert(snake.begin(),std::make_pair(snake[0].first,snake[0].second+1));
   
   	}
- 	else
+ 	else// to allow snake pass through one side of window and come out from other side
  	{
 
     		if(direction=='l')
@@ -250,7 +249,7 @@ void snakeclass::movesnake()
   }
 }
 
-void snakeclass::start()
+void snakeclass::start()// start execution from here
 {
 
 	clear();
@@ -289,7 +288,7 @@ void snakeclass::stage1()
 	getch();
 	refresh();
 	clear();
-
+//Put obstacles
 	for(int i=1;i<=3;i++)
 	{
 		move(i,10);
@@ -325,6 +324,7 @@ void snakeclass::stage1()
 	attron(A_BOLD);
 	move(maxheight-1,0);
 	printw("Your Score:%d",points);
+// if you surpass top score, then your score will become top score
 	if(points > high_score)
 	{
 		high_score=points;
@@ -349,7 +349,7 @@ void snakeclass::stage1()
  	while(1)
   	{
 
-		 if(coll==1)
+		 if(coll==1)// there was an collison. GAME OVER
    		{  
    			char poin[100],script[1024];
 			sprintf(poin,"%d",points);
@@ -359,7 +359,7 @@ void snakeclass::stage1()
 	 		break;
        		}
    	
-   		if(points==100)
+   		if(points==100)// move to stage 2 after you score 100 points
 		{
 			clear();
 			nodelay(stdscr,false);
@@ -371,7 +371,7 @@ void snakeclass::stage1()
 			getch();
 			attroff(A_BOLD);
 			nodelay(stdscr,true);
-			snakeclass s1(points,high_score);
+			snakeclass s1(points,high_score);// create object for stage 2
 			s1.stage2();
 			break;
 		}
@@ -381,7 +381,7 @@ void snakeclass::stage1()
 		if(direction=='q')
         		break;
 
-      		usleep(del);
+      		usleep(del);// delay
  
        }
   
@@ -399,12 +399,12 @@ void snakeclass::stage2()
 	refresh();
 	clear();
 	nodelay(stdscr,true);
-	
+	// remove obstacles of stage 1
 	while(!obst.empty())
 	{
 		obst.pop_back();
 	}
-
+// adding obstacles of stage 2
 	for(int i=1;i<=3;i++)
 	{
 		move(i+11,20-i);
@@ -491,11 +491,11 @@ void snakeclass::stage2()
 
 	while(1)
 	{
-		if(coll==1)
+		if(coll==1)// There was a collision. GAME OVER
    		{
    			char poin[100],script[1024];
         		sprintf(poin,"%d",points);
-        		strcpy(script,"/bin/bash script.sh ");
+        		strcpy(script,"/bin/bash script.sh ");// write top score to file
         		strcat(script,poin);
         		system(script);
 			break;
